167711b29b24a40c1bb2eb0e1c908047b37aaeae
BerreYesilyurt <77548130+BerreYesilyurt@users.noreply.github.com>
2021-09-09 00:02:01 +0300

    Update piechart.py

--- 08-09- Çarşamba/piechart.py
+++ 08-09- Çarşamba/piechart.py
@@ -20,6 +20,7 @@ with open(arguman[1]) as f:
 
 dizi=[] # Bu dizi JSON dosyadaki "Basılan tuş" veri bilgisinin alınabilmesi içindir
 basilantus=[]# # Bu ikinci diziyi basılan tuşlardan tekrarlı olanlarını bir kere tutmak için kullandım.
+basilantus1=[] # Basilan tus ifadesini silip sadece tuşun kalması için atanan dizi
 sayilar=[] # Bu dizi "Tuşun basılma sayısı" verisini tutar.
 basilmasayisi=[] # Birkaç kez basılan tuşların basılma sayısı da sayilar[] dizisinde birkaç kez tutuluyor. Bu verileri tekrarsız bir şekilde tutmak için olan dizi.
 explode=[0,0,0.1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] # Dairesel grafikte belirli dilimin daha belirgin durması için
@@ -38,9 +39,13 @@ for i in range(0,94):
     if(dizi[i] not in basilantus): # Burada amaç tekrarsız bir dizi oluşturmaktır. Bu yüzden if koşulunu kullandım. Eğer basilantus[] dizisinde bu eleman yoksa:
         basilantus.append(dizi[i]) # O zaman veryi basilantus[] dizisine ekle
         basilmasayisi.append(sayilar[i]) # Aynı zamanda basılan tuşun kaç kere basıldığına karşılık gelen ve yine aynı indekste bulunan basılma sayısını basilmasayisi[] dizisine ekle
+        
+for i in range(0,29): # basilantus[] dizisinin uzunluğu adeddince döndürdüm
+    basilantus1.append((basilantus[i][13:23])) # Sadece harflerin eklenmesi için yazdım
 
 
-plt.pie(basilmasayisi,labels=basilantus,explode=explode,wedgeprops={'edgecolor':'black'},radius=1.0,textprops={'fontsize':8,'color':"#065535"},rotatelabels=45, autopct='%1.1f%%')
+
+plt.pie(basilmasayisi,labels=basilantus1,explode=explode,wedgeprops={'edgecolor':'black'},radius=1.0,textprops={'fontsize':8,'color':"#065535"},rotatelabels=45, autopct='%1.1f%%')
  # Burada dairesel grafik çizimi ve ona dair özellikler var
 plt.title("Tuş Basma Dağılımı",color='#274E13',size=17) # Başlık yazısı, rengi ve büyüklüğü
 #plt.tight_layout()
